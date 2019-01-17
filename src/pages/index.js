4c18cffd13c3c1171d6813c6c4c716d3b640c000
calshius <callumfay_16@hotmail.co.uk>
2019-01-17 21:42:12 +0000

    Fixed missing dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,124 +16,154 @@ import full03 from '../assets/images/fulls/cybg.jpg'
 import full04 from '../assets/images/fulls/iomart.jpg'
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: full01, thumbnail: thumb01, caption: 'Barlcays | 2019-present: Assistant VP', description: 'Developer Experience & Cloud Toolchain Engineer'},
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'JPMC | 2017-2019: Associate', description: 'Cloud development & CI-CD'},
-    { id: '3', src: full03, thumbnail: thumb03, caption: 'CYBG | 2014-2017: Technical Specialist', description: 'Sysadmin & Microservice Development'},
-    { id: '4', src: full04, thumbnail: thumb04, caption: 'Iomart | 2012-2014: Systems Engineer', description: 'Cloud Sysadmin & NOC Engineering'}
-];
+  {
+    id: '1',
+    src: full01,
+    thumbnail: thumb01,
+    caption: 'Barlcays | 2019-present: Assistant VP',
+    description: 'Developer Experience & Cloud Toolchain Engineer',
+  },
+  {
+    id: '2',
+    src: full02,
+    thumbnail: thumb02,
+    caption: 'JPMC | 2017-2019: Associate',
+    description: 'Cloud development & CI-CD',
+  },
+  {
+    id: '3',
+    src: full03,
+    thumbnail: thumb03,
+    caption: 'CYBG | 2014-2017: Technical Specialist',
+    description: 'Sysadmin & Microservice Development',
+  },
+  {
+    id: '4',
+    src: full04,
+    thumbnail: thumb04,
+    caption: 'Iomart | 2012-2014: Systems Engineer',
+    description: 'Cloud Sysadmin & NOC Engineering',
+  },
+]
 
 class HomeIndex extends React.Component {
+  constructor() {
+    super()
 
-    constructor() {
-        super();
-
-        this.state = {
-            lightboxIsOpen: false,
-            currentImage: 0,
-        };
-
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-        this.handleClickImage = this.handleClickImage.bind(this);
+    this.state = {
+      lightboxIsOpen: false,
+      currentImage: 0,
     }
 
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    handleClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1) return;
+    this.closeLightbox = this.closeLightbox.bind(this)
+    this.gotoNext = this.gotoNext.bind(this)
+    this.gotoPrevious = this.gotoPrevious.bind(this)
+    this.openLightbox = this.openLightbox.bind(this)
+    this.handleClickImage = this.handleClickImage.bind(this)
+  }
 
-        this.gotoNext();
-    }
+  openLightbox(index, event) {
+    event.preventDefault()
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true,
+    })
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    })
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    })
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    })
+  }
+  handleClickImage() {
+    if (this.state.currentImage === this.props.images.length - 1) return
 
-    render() {
-        const siteTitle = "Callum Fay - portfolio"
-        const siteDescription = "Site description"
+    this.gotoNext()
+  }
 
-        return (
-            <Layout>
-                <Helmet>
-                        <title>{siteTitle}</title>
-                        <meta name="description" content={siteDescription} />
-                </Helmet>
+  render() {
+    const siteTitle = 'Callum Fay - portfolio'
+    const siteDescription = 'Site description'
 
-                <div id="main">
+    return (
+      <Layout>
+        <Helmet>
+          <title>{siteTitle}</title>
+          <meta name="description" content={siteDescription} />
+        </Helmet>
 
-                    <section id="one">
-                        <header className="major">
-                            <h2>Current proficiences listed below:</h2>
-                            </header>
-                            <table>
-                            <tbody>
-                            <tr>
-                            <td>&nbsp;<u>Cloud (on/off prem)</u></td>
-                            <td>&nbsp;AWS</td>
-                            <td>&nbsp;GKP&nbsp;</td>
-                            <td>&nbsp;AZURE</td>
-                            <td>&nbsp;K8s</td>
-                            <td>&nbsp;DCOS</td>
-                            <td>&nbsp;Openshift</td>
-                            </tr>
-                            <tr>
-                            <td>&nbsp;<u>Languages</u></td>
-                            <td>&nbsp;Python 2.7/3+&nbsp;</td>
-                            <td>&nbsp;Golang</td>
-                            <td>&nbsp;NodeJS</td>
-                            <td>&nbsp;Groovy</td>
-                            <td>&nbsp;Java 7/8&nbsp;</td>
-                            <td>&nbsp;ReactJS&nbsp;&nbsp;</td>
-                            </tr>
-                            <tr>
-                            <td>&nbsp;<u>Toolsets/Stacks</u></td>
-                            <td>&nbsp;ELK</td>
-                            <td>&nbsp;TICK&nbsp;</td>
-                            <td>&nbsp;Prometheus</td>
-                            <td>&nbsp;</td>
-                            <td>&nbsp;</td>
-                            <td>&nbsp;</td>
-                            </tr>
-                            </tbody>
-                            </table>
-                    </section>
+        <div id="main">
+          <section id="one">
+            <header className="major">
+              <h2>Current proficiences listed below:</h2>
+            </header>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    &nbsp;<u>Cloud (on/off prem)</u>
+                  </td>
+                  <td>&nbsp;AWS</td>
+                  <td>&nbsp;GKP&nbsp;</td>
+                  <td>&nbsp;AZURE</td>
+                  <td>&nbsp;K8s</td>
+                  <td>&nbsp;DCOS</td>
+                  <td>&nbsp;Openshift</td>
+                </tr>
+                <tr>
+                  <td>
+                    &nbsp;<u>Languages</u>
+                  </td>
+                  <td>&nbsp;Python 2.7/3+&nbsp;</td>
+                  <td>&nbsp;Golang</td>
+                  <td>&nbsp;NodeJS</td>
+                  <td>&nbsp;Groovy</td>
+                  <td>&nbsp;Java 7/8&nbsp;</td>
+                  <td>&nbsp;ReactJS&nbsp;&nbsp;</td>
+                </tr>
+                <tr>
+                  <td>
+                    &nbsp;<u>Toolsets/Stacks</u>
+                  </td>
+                  <td>&nbsp;ELK</td>
+                  <td>&nbsp;TICK&nbsp;</td>
+                  <td>&nbsp;Prometheus</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                  <td>&nbsp;</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
 
-                    <section id="two">
-                        <h2>Recent Work</h2>
+          <section id="two">
+            <h2>Recent Work</h2>
 
-                        <Gallery images={DEFAULT_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
-                            src,
-                            thumbnail,
-                            caption,
-                            description
-                        }))} />
-                    </section>
-
-                </div>
-
-            </Layout>
-        )
-    }
+            <Gallery
+              images={DEFAULT_IMAGES.map(
+                ({ id, src, thumbnail, caption, description }) => ({
+                  src,
+                  thumbnail,
+                  caption,
+                  description,
+                })
+              )}
+            />
+          </section>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default HomeIndex
